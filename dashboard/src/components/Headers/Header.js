0cb553c24d02a43859aade3933d3da60ac87c943
Diego Arteaga <148501504+diego-arteaga0@users.noreply.github.com>
2025-02-03 11:59:02 -0800

    Changed dashboard icons

--- dashboard/src/components/Headers/Header.js
+++ dashboard/src/components/Headers/Header.js
@@ -44,7 +44,7 @@ const Header = () => {
                       </div>
                       <Col className="col-auto">
                         <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
-                          <i className="fas fa-chart-bar" />
+                          <i className="fa-solid fa-droplet" />
                         </div>
                       </Col>
                     </Row>
@@ -72,7 +72,7 @@ const Header = () => {
                       </div>
                       <Col className="col-auto">
                         <div className="icon icon-shape bg-warning text-white rounded-circle shadow">
-                          <i className="fas fa-chart-pie" />
+                          <i className="fa-solid fa-cloud-showers-water" />
                         </div>
                       </Col>
                     </Row>
@@ -100,7 +100,7 @@ const Header = () => {
                       </div>
                       <Col className="col-auto">
                         <div className="icon icon-shape bg-yellow text-white rounded-circle shadow">
-                          <i className="fas fa-users" />
+                          <i className="fa-solid fa-temperature-half" />
                         </div>
                       </Col>
                     </Row>
@@ -128,13 +128,43 @@ const Header = () => {
                       </div>
                       <Col className="col-auto">
                         <div className="icon icon-shape bg-info text-white rounded-circle shadow">
-                          <i className="fas fa-percent" />
+                          <i className="ni ni-bulb-61" />
                         </div>
                       </Col>
                     </Row>
                     <p className="mt-3 mb-0 text-muted text-sm">
                       <span className="text-success mr-2">
                         <i className="fas fa-arrow-up" /> 12%
+                      </span>{" "}
+                      <span className="text-nowrap">Since last month</span>
+                    </p>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col lg="6" xl="3">
+                <Card className="card-stats mb-4 mb-xl-0">
+                  <CardBody>
+                    <Row>
+                      <div className="col">
+                        <CardTitle
+                          tag="h5"
+                          className="text-uppercase text-muted mb-0"
+                        >
+                          Soil pH
+                        </CardTitle>
+                        <span className="h2 font-weight-bold mb-0">
+                          3.4
+                        </span>
+                      </div>
+                      <Col className="col-auto">
+                        <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
+                          <i className="ni ni-sound-wave" />
+                        </div>
+                      </Col>
+                    </Row>
+                    <p className="mt-3 mb-0 text-muted text-sm">
+                      <span className="text-success mr-2">
+                        <i className="fa fa-arrow-up" /> 3.48%
                       </span>{" "}
                       <span className="text-nowrap">Since last month</span>
                     </p>
