045330928bb7316aca8f6f588731e05360ff69bf
Brianna Rangel <brianna.rangel.978@my.csun.edu>
2025-02-12 10:36:18 -0800

    log in edits

--- dashboard/src/layouts/Auth.js
+++ dashboard/src/layouts/Auth.js
@@ -58,13 +58,13 @@ const Auth = (props) => {
     <>
       <div className="main-content" ref={mainContent}>
         <AuthNavbar />
-        <div className="header bg-gradient-info py-7 py-lg-8">
+        <div className="header bg-light py-7 py-lg-8">
           <Container>
             <div className="header-body text-center mb-7">
               <Row className="justify-content-center">
                 <Col lg="5" md="6">
                   <h1 className="text-white">Welcome!</h1>
-                  <p className="text-lead text-light">
+                  <p className="text-lead text-white">
                     Log in here.
                   </p>
                 </Col>
@@ -80,10 +80,6 @@ const Auth = (props) => {
               x="0"
               y="0"
             >
-              <polygon
-                className="fill-default"
-                points="2560 0 2560 100 0 100"
-              />
             </svg>
           </div>
         </div>
